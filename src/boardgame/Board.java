c18a812537b82b5c3f18fe2f04a686a3c261d05f
BRIAN <owner@MBP-de-Brian.home>
2023-09-03 17:58:00 +0100

    Start Implemented Board

---
+++ src/boardgame/Board.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Board {
+  
+  private int rows;
+  private int columns;
+  private Piece[][] pieces;
+
+
+  public Board(int rows, int columns) {
+    this.rows = rows;
+    this.columns = columns;
+    pieces = new Piece[rows][columns];
+  }
+
+
+  public int getRow() {
+    return this.rows;
+  }
+
+  public void setRow(int rows) {
+    this.rows = rows;
+  }
+
+  public int getColumns() {
+    return this.columns;
+  }
+
+  public void setColumns(int columns) {
+    this.columns = columns;
+  }
+
+}
